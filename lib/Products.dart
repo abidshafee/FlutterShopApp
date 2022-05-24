8da59347762a69c7959b82a758b035d1529fe474
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-05-25 01:16:34 +0600

    init Commt

---
+++ lib/Products.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String image, title, description;
+  final int price, size, id;
+  final Color color;
+  Product({
+    this.image,
+    this.title,
+    this.description,
+    this.price,
+    this.size,
+    this.id,
+    this.color,
+  });
+}
+
+List<Product> products = [
+  Product(
+    id: 1,
+    title: "Product1",
+    description: productDesc,
+    price: 23,
+    size: 7,
+    image: "assets/images/p1.png"
+    color: Color(0xFF4A148C)),
+    Product(
+    id: 2,
+    title: "Product2",
+    description: productDesc,
+    price: 23,
+    size: 7,
+    image: "assets/images/p2.png"
+    color: Color(0xFF304FFE)),
+    Product(
+    id: 3,
+    title: "Product3",
+    description: productDesc,
+    price: 23,
+    size: 7,
+    image: "assets/images/p3.png"
+    color: Color(0xFF004D40)),
+    Product(
+    id: 4,
+    title: "Product4",
+    description: productDesc,
+    price: 23,
+    size: 7,
+    image: "assets/images/p4.png"
+    color: Color(0xFF1B5E20)),
+];
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/screens/home_screen.dart';
+import 'package:flutter_shop_app/constants.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Shop App',
+      theme: ThemeData(),
+      home: HomeScreen(),
+    );
+  }
+}
